e59a192c475bf1abe3c2b4173c03085c0492471a
eustus <mwirigieustus97@gmail.com>
2025-09-16 12:31:00 +0300

    feat(users): add custom user model, serializer, viewset, and routes

--- users/models.py
+++ users/models.py
@@ -6,6 +6,13 @@ class User(AbstractUser):
     Custom user model extending abstract user
     Adds available to save favourite models
     """
+    username = models.CharField(max_length=150,blank=True, unique=True)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
     favourites = models.ManyToManyField("movies.Movie", blank=True, related_name="favourited_by")
 
     def __str__(self):
